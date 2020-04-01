a0f9fec28a0809b6dade2cd91e02737edfd61b03
rahul <rahul@codemarshal.com>
2020-04-01 23:43:31 +0600

    first commit

---
+++ booklist/app.js
@@ -0,0 +1,90 @@
+
+// Book Constructor
+function Book(title,author,isbn){
+    this.title=title;
+    this.author=author;
+    this.isbn=isbn;
+}
+// UI Constructor
+function UI() {}
+
+UI.prototype.addBookToList=function(book){
+
+    const list=document.getElementById('book-list');
+    // Create tr Element
+    const row=document.createElement('tr');
+    //console.log(row);
+    //Insert Columns
+    row.innerHTML= `
+        <td> ${book.title}</td>
+        <td>${book.author}</td>
+        <td>${book.isbn}</td>
+        <td><a href="#" class="delete">X</a></td>
+    `;
+    list.appendChild(row);
+}
+
+// Clear All fields
+UI.prototype.clearFields=function(){
+    document.getElementById('title').value='';
+    document.getElementById('author').value='';
+    document.getElementById('isbn').value='';
+}
+
+// Show Alert
+UI.prototype.showAlert=function(message,className){
+    const div=document.createElement('div');
+    div.className=`alert ${className}`;
+    div.appendChild(document.createTextNode(message));
+    const container=document.querySelector('.container');
+    const form=document.querySelector('#book-form');
+    container.insertBefore(div,form);
+
+    // Timeout for 3 seconds
+    setTimeout(function(){
+        document.querySelector('.alert').remove();
+    }, 3000);
+}
+
+// Delete Book
+UI.prototype.deleteBook=function(target){
+    if(target.className==='delete'){
+        target.parentElement.parentElement.remove();
+    }
+}
+
+// Event Listener
+document.getElementById('book-form').addEventListener('submit',
+function(e){
+    const title=document.getElementById('title').value;
+    const author=document.getElementById('author').value;
+    const isbn=document.getElementById('isbn').value;
+
+    // Instantiate Book
+    const book=new Book(title,author,isbn);
+    
+    // Instantiate UI
+    const ui=new UI();
+    //console.log(UI);
+
+    // Validate
+    if(title==='' || author==='' || isbn===''){
+        //Error Alert
+        ui.showAlert('Please fill in all fields','error');
+    }
+    else{
+        ui.showAlert('Book Added!','success');
+        ui.addBookToList(book);
+        ui.clearFields();    
+    }
+
+    //console.log(book);
+    e.preventDefault();
+});
+
+document.getElementById('book-list').addEventListener('click', function(e){
+    const ui=new UI();
+    ui.deleteBook(e.target);
+    ui.showAlert('book removed','success');
+    e.preventDefault();
+});
